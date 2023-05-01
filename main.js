d33409c7685195eb9b8924ba7ccfd1aa70d56ca2
Paula <55994533+paulasilvanof@users.noreply.github.com>
2023-04-30 22:58:43 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        //alert('Elemento não encontrado');
+        console.log("Elemento não encontrado ou seletor inválido");
+    }  
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//Enquanto (usando o while)
+// let contador = 0;
+// while (contador < listaDeTeclas.length) {
+
+//     const tecla = listaDeTeclas[contador];
+//     const instrumento = tecla.classList[1];
+
+//     //Template string
+//     const idAudio = `#som_${instrumento}`;
+
+//     tecla.onclick = function () {
+//         tocaSom(idAudio);
+//     }
+
+//     contador = contador + 1;
+
+//     // console.log(contador);
+// }
+
+//Para (Usando o For)
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1]; 
+    const idAudio = `#som_${instrumento}`; //Template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento.code === 'Space') {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+        tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
+}
+
